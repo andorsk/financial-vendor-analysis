373ab81a0c944970e5415ac39f9bdfeb6398759f
Andor Kesselman <andor@andor.us>
2024-05-11 16:30:10 +0530

    change port to 5000

--- prototypes/backend/app.py
+++ prototypes/backend/app.py
@@ -155,4 +155,4 @@ def health():
     return jsonify({'status': 'OK'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
